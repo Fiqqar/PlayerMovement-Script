068eba434c950f5a84310114dd5ba8c8551f2bb6
Wfiq <145265758+Fiqqar@users.noreply.github.com>
2025-08-02 10:11:22 +0700

    Update Collectible.cs

--- Collectible.cs
+++ Collectible.cs
@@ -1,17 +1,30 @@
-
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    // Fungsi ini dipanggil Unity saat ada objek lain masuk ke trigger
+    public float respawnTime = 5f;
+    public Vector3 areaMin = new Vector3(-10, 0, -10);
+    public Vector3 areaMax = new Vector3(10, 0, 10);
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Beritahu GameManager untuk menambah skor
             GameManager.instance.AddScore(1);
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+            Invoke(nameof(Respawn), respawnTime);
         }
     }
-}
 
+    void Respawn()
+    {
+        Vector3 newPos = new Vector3(
+            Random.Range(areaMin.x, areaMax.x),
+            transform.position.y,
+            Random.Range(areaMin.z, areaMax.z)
+        );
+
+        transform.position = newPos;
+        gameObject.SetActive(true);
+    }
+}
